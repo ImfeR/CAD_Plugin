8d4ac6859d23aa1a343afd2132d0fecd8b2de5ff
ImfeR <imfer99@gmail.com>
2022-01-26 17:20:14 +0700

    Исправил замечания.

--- src/RocketPlugin.BL/GuidesType.cs
+++ src/RocketPlugin.BL/GuidesType.cs
@@ -1,10 +1,24 @@
 ﻿namespace RocketPlugin.BL
 {
     //TODO: XML
+    /// <summary>
+    /// Тип направляющей ракеты.
+    /// </summary>
     public enum GuidesType
     {
+        /// <summary>
+        /// Трапециевидный
+        /// </summary>
         Trapezoid = 0,
+        
+        /// <summary>
+        /// Прямоугольный
+        /// </summary>
         Rectangle = 1,
+        
+        /// <summary>
+        /// Треугольный
+        /// </summary>
         Triangle = 2
     }
 }
